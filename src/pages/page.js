83376673e4446b15854911177af6be495de37109
zuhayrhasan <zuhxyr@gmail.com>
2023-08-05 01:02:04 -0400

    experience page

--- src/pages/page.js
+++ src/pages/page.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from 'react';
 import Header from '../components/header.js';
 import '../styles/landingpage.css';
 import '../styles/projectspage.css';
+import '../styles/experiencepage.css';
+import '../styles/skillspage.css';
+import '../styles/contactpage.css';
 
 const Page = () => {
 
@@ -46,6 +49,33 @@ const Page = () => {
         <section id="projects-page" class="projects-page">
             <Header id="header" HeaderName="Projects" delay="2500"></Header>
         </section>
+        <section id="experience-page" class="experience-page">
+            <Header id="header" HeaderName="Experience" delay="2500"></Header>
+            <div class="experience-cont">
+                <h3 class="h3-underline-text">Equestrian Show Manager</h3>
+                <h5>May 2022 — August 2022</h5>
+                <div class="row-of-skills">
+                    <h5 class="skill">Ruby</h5>
+                    <h5 class="skill">Ruby on Rails</h5>
+                    <h5 class="skill">PostgreSQL</h5>
+                    <h5 class="skill">Ubuntu</h5>
+                </div>
+                <div class="experience-text-box">
+                    <p>Demonstrated <span class="underline-text">quick learning skills</span> by acquiring proficiency in Ruby within a week and successfully applying it to
+                        accomplish various tasks, such as  <span class="underline-text">fetching accurate data</span> from PostgreSQL, <span class="underline-text">authenticating user information</span>, and <span class="underline-text"> creating logical</span> search queries</p>
+                    <p><span class="underline-text">Collaborated</span> with a team of 4 engineers to <span class="underline-text">review and troubleshoot work</span>, leading to enhanced learning and
+                        improved outcomes through teamwork</p>
+                    <p>Re-organized 8 web pages and re-factored PDF reports using Ruby on Rails to create a <span class="underline-text">better user experience</span></p>
+                </div>
+            </div>
+        </section>
+        <section id="skills-page" class="skills-page">
+            <Header id="header" HeaderName="Skills" delay="2500"></Header>
+        </section>
+        <section id="contact-page" class="contact-page">
+            <Header id="header" HeaderName="Contact Me" delay="2500"></Header>
+        </section>
+        
       </>
     );
 }
